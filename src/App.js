5645da352d817ad4d7f34143360da4cca7ab2744
Sean Leif Hellier <sean@newtropolis.com>
2020-08-14 09:10:03 -0400

    added and confirmed Alan-AI functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import alanBtn from "@alan-ai/alan-sdk-web";
 const alanKey =
 	"47a7b11eeb3cc7622ced78a9dcf06b5b2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
-// head position 11:08
+// head position 19:16
 
 const App = () => {
 	useEffect(() => {
-		key: alanKey;
+		alanBtn({
+			key: alanKey,
+			onCommand: ({ command }) => {
+				if (command === "testCommand") {
+					alert("testCommand was executed");
+				}
+			},
+		});
 	}, []);
 
 	return (
