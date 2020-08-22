26d43d25a76972f8bdc254844685eceb94cdd71c
Sean Leif Hellier <sean@newtropolis.com>
2020-08-22 13:16:30 -0400

    added alan-ai source code (voice controller.js)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/newsCards/newsCards";
+import useStyles from "./styles.js";
 
 const alanKey =
 	"47a7b11eeb3cc7622ced78a9dcf06b5b2e956eca572e1d8b807a3e2338fdd0dc/stage";
@@ -11,6 +12,7 @@ const alanKey =
 
 const App = () => {
 	const [newsArticles, setNewsArticles] = useState([]);
+	const classes = useStyles();
 	useEffect(() => {
 		alanBtn({
 			key: alanKey,
@@ -24,6 +26,16 @@ const App = () => {
 	}, []);
 
 	return (
+		// <div>
+		// 	<div className={classes.logoContainer}>
+		// 		<img
+		// 			src="../img/news-alt.png"
+		// 			className={classes.projectLogo}
+		// 			alt="project logo"
+		// 		/>
+		// 		<NewsCards articles={newsArticles} />
+		// 	</div>
+
 		<div>
 			<h1>Voice Controller</h1>
 			<NewsCards articles={newsArticles} />
