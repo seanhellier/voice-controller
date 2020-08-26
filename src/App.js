948d11b6756b29a11d25cc37b17c8bee30d0ccb3
Sean Leif Hellier <sean@newtropolis.com>
2020-08-26 17:22:12 -0400

    added wordsToNumbers for article play control

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/newsCards/newsCards";
 import useStyles from "./styles.js";
+import wordsToNumbers from "words-to-numbers"; 
 
 const alanKey =
 	"47a7b11eeb3cc7622ced78a9dcf06b5b2e956eca572e1d8b807a3e2338fdd0dc/stage";
@@ -25,6 +26,8 @@ const App = () => {
 				} else if (command === "highlight") {
 					setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
 				} else if (command === "open") {
+
+					const parsedNumber = numberl.length > 2 wordsToNumbers() : number;
 					console.log(number);
 					window.open(articles[number].url, "_blank");
 				}
