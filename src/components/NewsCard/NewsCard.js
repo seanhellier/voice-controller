3e2adb0ca327e72245d18d3ddd244c831f76e516
Sean Leif Hellier <sean@newtropolis.com>
2020-08-26 07:38:50 -0400

    added vertical scroll via elRef

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -20,18 +20,21 @@ const NewsCard = ({
 }) => {
 	const classes = useStyles();
 	const [elRefs, setElRefs] = useState([]);
+	const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50)
 
 	useEffect(() => {
 		setElRefs((refs) => Array(20).fill().map(_ , j) => refs[j] || createRef()));
 	}, []);
 
 	useEffect(() => {
-
+		if(i === activeArticle && elRefs[activeArticle]) {
+			scrollToRef(elRefs[activeArticle]);
+		}
 	}, [ i, activeArticle, elRefs]
 	)
 
 	return (
-		<Card
+		<Card ref={elRefs[i]}
 			className={classNames(
 				classes.card,
 				activeArticle === i ? classes.activeCard : null
